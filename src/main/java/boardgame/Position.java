798d542e5fd166a4c9cddf0f25e13f48a9efb93a
Vinicius Soares <viniciussoaresaqw@hotmail.com>
2022-04-16 08:56:47 -0300

    First class: Position

---
+++ src/main/java/boardgame/Position.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position(Integer row, Integer column) {
+        setRow(row);
+        setColumn(column);
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+    public Integer getColumn() {
+        return column;
+    }
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return this.row + ", " + this.column;
+    }
+}
--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -1,7 +1,11 @@
 package program;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] arg){
-        
+        Position position = new Position(5,3);
+
+        System.out.println(position);
     }
 }
